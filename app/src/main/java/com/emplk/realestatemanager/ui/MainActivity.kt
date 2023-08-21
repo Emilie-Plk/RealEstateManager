b753a14a2962367bdd91c5bcda590506f528c8a8
Émilie P <emiliep.dev@gmail.com>
2023-08-21 16:35:08 +0200

    Base project: ViewBinding in place, dimens, color values, and property item xml

---
+++ app/src/main/java/com/emplk/realestatemanager/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.emplk.realestatemanager.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.emplk.realestatemanager.databinding.MainActivityBinding
+import com.emplk.realestatemanager.ui.utils.viewBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding { MainActivityBinding.inflate(it) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/emplk/realestatemanager/ui/utils/Event.kt
@@ -0,0 +1,32 @@
+package com.emplk.realestatemanager.ui.utils
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+
+data class Event<out T>(private val content: T) {
+
+    private var handled = false
+
+    // Keep this function private to force the use of the observeEvent extension
+    private fun getContentIfNotHandled(): T? = if (handled) {
+        null
+    } else {
+        handled = true
+        content
+    }
+
+    companion object {
+        /**
+         * An extension for [Event]s, simplifying the pattern of checking if the [Event]'s content has already been handled.
+         *
+         * [onEventUnhandledContent] is *only* called if the [Event]'s contents has not been handled.
+         */
+        fun <T> LiveData<Event<T>>.observeEvent(owner: LifecycleOwner, onEventUnhandledContent: (T) -> Unit) {
+            observe(owner) { event ->
+                event.getContentIfNotHandled()?.let {
+                    onEventUnhandledContent(it)
+                }
+            }
+        }
+    }
+}
