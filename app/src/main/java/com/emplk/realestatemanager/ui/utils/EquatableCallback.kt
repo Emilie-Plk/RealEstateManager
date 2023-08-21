c3816114a5ab3bc2e4d46008dad1228d4e3f803f
Émilie P <emiliep.dev@gmail.com>
2023-08-21 16:35:08 +0200

    Base project: ViewBinding in place, dimens, color values, and property item xml

---
+++ app/src/main/java/com/emplk/realestatemanager/ui/utils/EquatableCallback.kt
@@ -0,0 +1,16 @@
+package com.emplk.realestatemanager.ui.utils
+
+class EquatableCallback(private val callback: () -> Unit) {
+
+    operator fun invoke() {
+        callback.invoke()
+    }
+
+    override fun equals(other: Any?): Boolean = if (other is EquatableCallback) {
+        true
+    } else {
+        super.equals(other)
+    }
+
+    override fun hashCode(): Int = 2051656923
+}
