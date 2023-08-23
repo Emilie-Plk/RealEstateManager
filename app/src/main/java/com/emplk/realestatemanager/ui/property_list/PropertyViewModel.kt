357ee327635aff747c5850ce3c5423c7f43271ff
Émilie P <emiliep.dev@gmail.com>
2023-08-23 21:54:24 +0200

    Starting to setup MainActivity logic

--- app/src/main/java/com/emplk/realestatemanager/ui/property_list/PropertyViewModel.kt
+++ app/src/main/java/com/emplk/realestatemanager/ui/property_list/PropertyViewModel.kt
@@ -1,11 +1,12 @@
 package com.emplk.realestatemanager.ui.property_list
 
 import androidx.lifecycle.ViewModel
+import com.emplk.realestatemanager.domain.get_properties.GetPropertiesAsFlowUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class PropertyViewModel @Inject constructor(
-
+private val getPropertiesAsFlowUseCase: GetPropertiesAsFlowUseCase,
 ) : ViewModel() {
 }
