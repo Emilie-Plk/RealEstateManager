5398cabe10331ed2942df926b61ddd539c36852b
Émilie P <emiliep.dev@gmail.com>
2023-08-23 21:54:24 +0200

    Starting to setup MainActivity logic

--- app/src/main/java/com/emplk/realestatemanager/ui/main/MainViewModel.kt
+++ app/src/main/java/com/emplk/realestatemanager/ui/main/MainViewModel.kt
@@ -1,9 +1,23 @@
 package com.emplk.realestatemanager.ui.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.emplk.realestatemanager.domain.get_properties.GetPropertiesAsFlowUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
-//@HiltViewModel
-class MainViewModel {
+@HiltViewModel
+class MainViewModel @Inject constructor(
+) : ViewModel() {
+
+    private val isTabletMutableStateFlow = MutableStateFlow(false)
+
+    val isTabletLiveData = isTabletMutableStateFlow.asStateFlow()
+
+    fun onResume(isTablet: Boolean) {
+        isTabletMutableStateFlow.value = isTablet
+        Log.d("COUCOU", "onResume: isTablet = $isTablet")
+    }
 }
