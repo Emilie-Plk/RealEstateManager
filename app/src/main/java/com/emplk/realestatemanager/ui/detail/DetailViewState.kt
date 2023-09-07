c9aecd5b06d1e44bd5814d0cdc41807b84944f2f
Émilie P <emiliep.dev@gmail.com>
2023-09-07 10:23:09 +0200

    Added progressbar to Details too, but not optimal

--- app/src/main/java/com/emplk/realestatemanager/ui/detail/DetailViewState.kt
+++ app/src/main/java/com/emplk/realestatemanager/ui/detail/DetailViewState.kt
@@ -3,29 +3,39 @@ package com.emplk.realestatemanager.ui.detail
 import com.emplk.realestatemanager.ui.utils.NativePhoto
 import com.emplk.realestatemanager.ui.utils.NativeText
 
-data class DetailViewState(
-    val id: Long,
-    val type: String,
-    val featuredPicture: NativePhoto,
-    val pictures: List<String>,
-    val price: NativeText,
-    val surface: NativeText,
-    val rooms: NativeText,
-    val bathrooms: NativeText,
-    val bedrooms: NativeText,
-    val description: String,
-    val address: NativeText,
-    val amenitySchool: Boolean,
-    val amenityPark: Boolean,
-    val amenityShoppingMall: Boolean,
-    val amenityRestaurant: Boolean,
-    val amenityConcierge: Boolean,
-    val amenityPublicTransportation: Boolean,
-    val amenityHospital: Boolean,
-    val amenityLibrary: Boolean,
-    val amenityGym: Boolean,
-    val entryDate: NativeText,
-    val agentName: NativeText,
-    val isSold: Boolean,
-    val saleDate: NativeText?,
-)
+sealed class DetailViewState(val type: Type) {
+
+    enum class Type {
+        PROPERTY_DETAIL,
+        LOADING,
+    }
+
+    data class PropertyDetail(
+        val id: Long,
+        val propertyType: String,
+        val featuredPicture: NativePhoto,
+        val pictures: List<String>,
+        val price: NativeText,
+        val surface: NativeText,
+        val rooms: NativeText,
+        val bathrooms: NativeText,
+        val bedrooms: NativeText,
+        val description: String,
+        val address: NativeText,
+        val amenitySchool: Boolean,
+        val amenityPark: Boolean,
+        val amenityShoppingMall: Boolean,
+        val amenityRestaurant: Boolean,
+        val amenityConcierge: Boolean,
+        val amenityPublicTransportation: Boolean,
+        val amenityHospital: Boolean,
+        val amenityLibrary: Boolean,
+        val amenityGym: Boolean,
+        val entryDate: NativeText,
+        val agentName: NativeText,
+        val isSold: Boolean,
+        val saleDate: NativeText?,
+    ) : DetailViewState(Type.PROPERTY_DETAIL)
+
+    object LoadingState : DetailViewState(Type.LOADING)
+}
