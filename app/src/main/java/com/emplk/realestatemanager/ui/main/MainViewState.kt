f8807d11939502a02e0d78e492591b20782099d2
Émilie P <emiliep.dev@gmail.com>
2023-09-11 14:13:09 +0200

    Refacto/Added MainViewState to manage state and view for Main (appbar, buttons)

---
+++ app/src/main/java/com/emplk/realestatemanager/ui/main/MainViewState.kt
@@ -0,0 +1,8 @@
+package com.emplk.realestatemanager.ui.main
+
+data class MainViewState(
+    val isAddFabVisible: Boolean,
+    val isFilterAppBarButtonVisible: Boolean,
+    val isAddAppBarButtonVisible: Boolean,
+    val subtitle: String?,
+)
