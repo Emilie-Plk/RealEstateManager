47b10ca1138fb0f338e50447fdcd35f861ff8c1e
Émilie P <emiliep.dev@gmail.com>
2023-09-11 11:17:02 +0200

    Added PropertyPicturePreviewListAdapter + changed xml for picture preview

--- app/src/main/java/com/emplk/realestatemanager/ui/add/picture_preview/PicturePreviewStateItem.kt
+++ app/src/main/java/com/emplk/realestatemanager/ui/add/picture_preview/PicturePreviewStateItem.kt
@@ -1,10 +1,11 @@
 package com.emplk.realestatemanager.ui.add.picture_preview
 
 import com.emplk.realestatemanager.ui.utils.EquatableCallback
+import com.emplk.realestatemanager.ui.utils.NativePhoto
 
 data class PropertyPicturePreviewStateItem(
     val id : Long,
-    val uri : String,
+    val uri : NativePhoto,
     val description : String,
     val isFeature : Boolean,
     val onDeleteEvent: EquatableCallback,
