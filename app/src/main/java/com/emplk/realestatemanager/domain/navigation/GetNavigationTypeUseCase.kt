9548abbd274d6ea436f3275a3d6ecae34ab6569d
Émilie P <emiliep.dev@gmail.com>
2023-09-14 12:12:44 +0200

    Refactored Nav by adding displayFragment method, a bit cleaner (still broken)

--- app/src/main/java/com/emplk/realestatemanager/domain/navigation/GetNavigationTypeUseCase.kt
+++ app/src/main/java/com/emplk/realestatemanager/domain/navigation/GetNavigationTypeUseCase.kt
@@ -1,6 +1,7 @@
 package com.emplk.realestatemanager.domain.navigation
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 class GetNavigationTypeUseCase @Inject constructor(
