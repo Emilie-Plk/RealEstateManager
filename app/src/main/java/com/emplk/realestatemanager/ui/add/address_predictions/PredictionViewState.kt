476d7d8ef2ebe6a060ed4e2510d6219810ab94ca
Émilie P <emiliep.dev@gmail.com>
2023-09-20 18:30:44 +0200

    Added predictionListAdapter + before mentoring session!

---
+++ app/src/main/java/com/emplk/realestatemanager/ui/add/address_predictions/PredictionViewState.kt
@@ -0,0 +1,22 @@
+package com.emplk.realestatemanager.ui.add.address_predictions
+
+import com.emplk.realestatemanager.ui.property_list.PropertiesViewState
+import com.emplk.realestatemanager.ui.utils.EquatableCallback
+
+sealed class PredictionViewState(val type: Type) {
+
+    enum class Type {
+        PREDICTION,
+        EMPTY_STATE,
+    }
+
+    data class Prediction(
+        val id: Long,
+        val description: String,
+        val placeId: String,
+        val onClickEvent: EquatableCallback,
+    ) : PredictionViewState(Type.PREDICTION)
+
+    object EmptyState : PredictionViewState(Type.EMPTY_STATE)
+}
+
