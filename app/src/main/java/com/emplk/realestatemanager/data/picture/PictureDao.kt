a663a1a94549de0ca1125ac207212a828874563d
Émilie P <emiliep.dev@gmail.com>
2023-09-20 18:04:46 +0200

    Modified addPropertyWithDetails in PropertyRepoRoom (whoopsie, forgot to fix this up!)

--- app/src/main/java/com/emplk/realestatemanager/data/picture/PictureDao.kt
+++ app/src/main/java/com/emplk/realestatemanager/data/picture/PictureDao.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.Flow
 interface PictureDao {
 
     @Insert
-    suspend fun insert(pictureDto: PictureDto): Long
+    suspend fun insert(pictureDto: PictureDto): Long?
 
     @Query("SELECT * FROM pictures WHERE property_id = :propertyId")
     fun getPicturesAsFlow(propertyId: Long): Flow<List<PictureDto>>
