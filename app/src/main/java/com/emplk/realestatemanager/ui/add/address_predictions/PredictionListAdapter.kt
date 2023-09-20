8b425a421de82c3853278277873c7167875729a4
Émilie P <emiliep.dev@gmail.com>
2023-09-20 18:30:44 +0200

    Added predictionListAdapter + before mentoring session!

---
+++ app/src/main/java/com/emplk/realestatemanager/ui/add/address_predictions/PredictionListAdapter.kt
@@ -0,0 +1,63 @@
+package com.emplk.realestatemanager.ui.add.address_predictions
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.emplk.realestatemanager.databinding.PropertyAddressPredictionEmptyBinding
+import com.emplk.realestatemanager.databinding.PropertyAddressPredictionItemBinding
+
+class PredictionListAdapter :
+    ListAdapter<PredictionViewState, PredictionListAdapter.PredictionViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PredictionViewHolder =
+        when (PredictionViewState.Type.values()[viewType]) {
+            PredictionViewState.Type.PREDICTION -> PredictionViewHolder.Prediction.create(parent)
+            PredictionViewState.Type.EMPTY_STATE -> PredictionViewHolder.EmptyState.create(parent)
+        }
+
+    override fun onBindViewHolder(holder: PredictionViewHolder, position: Int) {
+        when (holder) {
+            is PredictionViewHolder.Prediction -> holder.bind(item = getItem(position) as PredictionViewState.Prediction)
+            is PredictionViewHolder.EmptyState -> Unit
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int = getItem(position).type.ordinal
+
+    sealed class PredictionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        class Prediction(private val binding: PropertyAddressPredictionItemBinding) :
+            PredictionViewHolder(binding.root) {
+            companion object {
+                fun create(parent: ViewGroup) = Prediction(
+                    binding = PropertyAddressPredictionItemBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+
+            fun bind(item: PredictionViewState.Prediction) {
+                binding.root.setOnClickListener { item.onClickEvent.invoke() }
+                binding.addPropertySuggestionTextView.text = item.description
+            }
+        }
+
+        class EmptyState(private val binding: PropertyAddressPredictionEmptyBinding) : RecyclerView.ViewHolder(binding.root) {
+            companion object {
+                fun create(parent: ViewGroup) = EmptyState(
+                    binding = PropertyAddressPredictionEmptyBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+        }
+    }
+
+
+}
