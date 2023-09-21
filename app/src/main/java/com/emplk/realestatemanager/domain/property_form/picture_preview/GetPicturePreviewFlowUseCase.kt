15a748997a8afee865c6652e78ec6ac40c4d561a
Émilie P <emiliep.dev@gmail.com>
2023-09-21 12:22:34 +0200

    Got rid of useless UseCases from Form creation

--- app/src/main/java/com/emplk/realestatemanager/domain/property_form/picture_preview/GetPicturePreviewFlowUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.emplk.realestatemanager.domain.property_form.picture_preview
-
-import javax.inject.Inject
-
-class GetPicturePreviewFlowUseCase @Inject constructor(
-    private val picturePreviewRepository: PicturePreviewRepository,
-) {
-    fun invoke() = picturePreviewRepository.getAsFlow()
-}
