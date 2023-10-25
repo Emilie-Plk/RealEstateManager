6fd8c7de54ee5064cfdd014dedc22a7a5581373e
Émilie P <emiliep.dev@gmail.com>
2023-10-25 12:22:19 +0200

    Added Dialog Fragment to enter title when adding new form

---
+++ app/src/main/java/com/emplk/realestatemanager/ui/drafts/DraftsViewModel.kt
@@ -0,0 +1,7 @@
+package com.emplk.realestatemanager.ui.drafts
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class DraftsViewModel @Inject constructor() : ViewModel() {
+}
---
+++ app/src/main/java/com/emplk/realestatemanager/ui/drafts/add_form_dialog/AddNewFormDialogFragment.kt
@@ -0,0 +1,17 @@
+package com.emplk.realestatemanager.ui.drafts.add_form_dialog
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AddNewFormDialogFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
