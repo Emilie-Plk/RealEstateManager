c3d8914fe007729a0fb9ae4e365815c2b052ef07
Émilie P <emiliep.dev@gmail.com>
2023-10-25 12:22:19 +0200

    Added Dialog Fragment to enter title when adding new form

--- app/src/main/java/com/emplk/realestatemanager/ui/drafts/DraftsFragment.kt
+++ app/src/main/java/com/emplk/realestatemanager/ui/drafts/DraftsFragment.kt
@@ -2,17 +2,23 @@ package com.emplk.realestatemanager.ui.drafts
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.emplk.realestatemanager.R
+import com.emplk.realestatemanager.databinding.DraftsFragmentBinding
+import com.emplk.realestatemanager.ui.utils.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class DraftsFragment : Fragment(R.layout.drafts_fragment) {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.drafts_fragment, container, false)
+
+    private val binding by viewBinding { DraftsFragmentBinding.bind(it) }
+    private val viewModel by viewModels<DraftsViewModel>()
+
+    companion object {
+        fun newInstance() = DraftsFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 }
