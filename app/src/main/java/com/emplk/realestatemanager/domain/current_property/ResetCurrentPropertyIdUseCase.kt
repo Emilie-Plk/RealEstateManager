1520aed424dfb56f2bebecf9b52f407d4851fc86
Émilie P <emiliep.dev@gmail.com>
2023-11-04 17:59:23 +0100

    Added InputFilterMinMax for Loan Sim loan rate

--- app/src/main/java/com/emplk/realestatemanager/domain/current_property/ResetCurrentPropertyIdUseCase.kt
+++ app/src/main/java/com/emplk/realestatemanager/domain/current_property/ResetCurrentPropertyIdUseCase.kt
@@ -5,7 +5,7 @@ import javax.inject.Inject
 class ResetCurrentPropertyIdUseCase @Inject constructor(
     private val currentPropertyRepository: CurrentPropertyRepository,
 ) {
-    fun invoke() {  // TODO : revoir et ptet juste m'en servir aussi nav back
+    fun invoke() {
         currentPropertyRepository.resetCurrentPropertyId()
     }
 }
--- app/src/main/java/com/emplk/realestatemanager/ui/utils/DecimalDigitsInputFilter.kt
+++ app/src/main/java/com/emplk/realestatemanager/ui/utils/DecimalDigitsInputFilter.kt
@@ -1,16 +1,18 @@
+package com.emplk.realestatemanager.ui.utils
+
 import android.text.InputFilter
 import android.text.Spanned
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
-class DecimalDigitsInputFilter( maxDecimalPlaces: Int) : InputFilter {
+class DecimalDigitsInputFilter(maxDecimalPlaces: Int) : InputFilter {
     private val pattern: Pattern = Pattern.compile(
         "[0-9]"  + "+((\\.[0-9]{0,"
                 + (maxDecimalPlaces - 1) + "})?)||(\\.)?"
     )
 
     override fun filter(
-        p0: CharSequence?,p1: Int,p2: Int,p3: Spanned?,p4: Int,p5: Int
+        p0: CharSequence?, p1: Int, p2: Int, p3: Spanned?, p4: Int, p5: Int
     ): CharSequence? {
         p3?.apply {
             val matcher: Matcher = pattern.matcher(p3)
